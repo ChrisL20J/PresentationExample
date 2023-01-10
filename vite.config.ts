466f177faffa496ad122834e76e3f58e108a9776
Chris Lam <chrislam.fw@gmail.com>
2023-01-10 17:15:44 +0800

    change description

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ import LinkAttributes from 'markdown-it-link-attributes'
 import Unocss from 'unocss/vite'
 import Shiki from 'markdown-it-shiki'
 import VueMacros from 'unplugin-vue-macros/vite'
+import HighchartsVue from 'highcharts-vue'
 
 export default defineConfig({
   resolve: {
