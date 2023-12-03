0bdf3adf24a7bcd52a321676ca99986cd9608ef0
ChrisL20J <chrislam.fw@gmail.com>
2023-12-03 15:52:49 +0800

    some update

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,6 +2,9 @@
 const { t, locale } = useI18n()
 
 const targetLocale = ref('')
+const show_me = ref(false)
+const show_mo = ref(false)
+const show_lf = ref(false)
 
 watch(targetLocale, () => {
   locale.value = targetLocale.value
@@ -14,9 +17,50 @@ watch(targetLocale, () => {
       <svg width="1em" height="1em"><path fill="currentColor" d="M4 21V9l8-6l8 6v12h-6v-7h-4v7Z" /></svg>
     </RouterLink>
 
-    <RouterLink class="icon-btn mx-2 hover:bg-gray hover:text-black" rel="noreferrer" to="/AUC">
-      ROC & AUC
-    </RouterLink>
+    <div class="icon-btn mx-2">
+      <div class="icon-btn mx-2 hover:bg-gray hover:text-black" @click="show_me = !show_me">
+        Metrics
+      </div>
+      <div v-if="show_me">
+        <ul>
+          <li>
+            <RouterLink class="icon-btn hover:bg-gray hover:text-black" rel="noreferrer" to="/AUC" @click="show_me = !show_me">
+              ROC & AUC
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="icon-btn mx-2">
+      <div class="icon-btn mx-2 hover:bg-gray hover:text-black" @click="show_lf = !show_lf">
+        Loss Function
+      </div>
+      <div v-if="show_lf">
+        <ul>
+          <li>
+            <RouterLink class="icon-btn hover:bg-gray hover:text-black" rel="noreferrer" to="" @click="show_lf = !show_lf">
+              coming soon
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="icon-btn mx-2">
+      <div class="icon-btn mx-2 hover:bg-gray hover:text-black" @click="show_mo = !show_mo">
+        Model
+      </div>
+      <div v-if="show_mo">
+        <ul>
+          <li>
+            <RouterLink class="icon-btn hover:bg-gray hover:text-black" rel="noreferrer" to="" @click="show_mo = !show_mo">
+              coming soon
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
 
     <select v-model="targetLocale" class="float-right">
       <option disabled value="">
